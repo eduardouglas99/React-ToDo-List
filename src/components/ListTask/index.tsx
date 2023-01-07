4d4ee365ce45ccea68b538a9b844d659373d8b6d
Eduardo Douglas de Melo Caetano <ecaetano@marlin.net>
2023-01-07 19:12:41 -0300

    resolucao das funcoes de limpar tarefas e suas modais

--- src/components/ListTask/index.tsx
+++ src/components/ListTask/index.tsx
@@ -1,5 +1,6 @@
 import styles from './ListTask.module.scss';
 import { FaTrash } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
 
 type ListTaskProps = {
     ListTask: string[];
@@ -8,13 +9,44 @@ type ListTaskProps = {
 }
 
 export default function ListTask({ListTask, clearTask, clearAllTasks} :ListTaskProps) {
+    const [itemParaExcluir, setItemParaExcluir] = useState<number>();
+    const [openModalLimpar, setOpenModalLimpar] = useState<boolean>(false);
+
     return (
         <>
+            {(typeof itemParaExcluir == "number" || (openModalLimpar && ListTask)) && (
+                <div className={`modalConfirmClearTask`}>
+                    <h2>Aviso</h2>
+                    <p>
+                        {!openModalLimpar ? `Realmente deseja excluir essa tarefa?` : `Realmente deseja excluir todos as tarefas?`}
+                    </p>
+                    
+                    <button type='button' onClick={() => {
+                        if(!openModalLimpar && typeof itemParaExcluir == "number") {
+                            clearTask(itemParaExcluir);
+                            setItemParaExcluir(undefined);
+                            return;
+                        }
+                        clearAllTasks();
+                        setOpenModalLimpar(false)
+                    }}>
+                        Sim
+                    </button>
+                    <button type='button' onClick={() => {
+                        setItemParaExcluir(undefined);
+                        setOpenModalLimpar(false)
+                    }}>
+                        Não
+                    </button>
+                </div>
+            )}
             <ul className={`${styles.listaItem} flex`}>
                 {ListTask.map((item, index) => (
                     <li key={index} className={`flex`}>
                         {item}
-                        <button type='button' className={`flex`} onClick={() => clearTask(index)}>
+                        <button type='button' className={`flex`} onClick={() => {
+                            setItemParaExcluir(index);
+                        }}>
                             <FaTrash />
                         </button>
                     </li>
@@ -22,7 +54,13 @@ export default function ListTask({ListTask, clearTask, clearAllTasks} :ListTaskP
             </ul>
             <div className={`${styles.buttonLimpar} flex`}>
                 <p>Você possui {ListTask.length === 1 ? `${ListTask.length} tarefa` : `${ListTask.length} tarefas`}</p>
-                <button type='button' onClick={() => clearAllTasks()}>Limpar</button>
+                <button type='button' onClick={() => {
+                    if(ListTask.length > 0) {
+                        setOpenModalLimpar(true);
+                        return;
+                    }
+                    alert("Você não possui tarefas para excluir :/")
+                }}>Limpar</button>
             </div>
         </>
     )
